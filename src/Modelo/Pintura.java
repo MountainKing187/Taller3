09543f10030abf7b26ad5e0f297a3b282c2ed03d
Nyest <g.vergara05@ufromail.cl>
2026-06-26 14:13:09 -0400

    Creado clases de entidades

--- src/Modelo/Pintura.java
+++ src/Modelo/Pintura.java
@@ -8,51 +8,17 @@ public class Pintura {
     private String estado;
     private int codAutor;
 
-    public int getAnio() {
-        return anio;
-    }
+    public int getAnio() { return anio; }
+    public String getEstado() { return estado; }
+    public int getCodAutor() { return codAutor; }
+    public int getCodPintura() { return codPintura; }
+    public String getTitulo() { return titulo; }
+    public int getValor() { return valor; }
 
-    public String getEstado() {
-        return estado;
-    }
-    
-    public int getCodAutor() {
-        return codAutor;
-    }
-    
-    public int getCodPintura() {
-        return codPintura;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public int getValor() {
-        return valor;
-    }
-
-    public void setAnio(int anio) {
-        this.anio = anio;
-    }
-
-    public void setCodAutor(int codAutor) {
-        this.codAutor = codAutor;
-    }
-
-    public void setCodPintura(int codPintura) {
-        this.codPintura = codPintura;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-    
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public void setValor(int valor) {
-        this.valor = valor;
-    }
+    public void setAnio(int anio) { this.anio = anio; }
+    public void setCodAutor(int codAutor) { this.codAutor = codAutor; }
+    public void setCodPintura(int codPintura) { this.codPintura = codPintura; }
+    public void setEstado(String estado) { this.estado = estado; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public void setValor(int valor) { this.valor = valor;}
 }
